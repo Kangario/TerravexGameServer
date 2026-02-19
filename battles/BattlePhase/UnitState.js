ccbc1e816f932b29bfe70d5030d9103e92b74316
Alexander Trushkov <mr.trushkoff2016@mail.ru>
2026-02-19 13:04:26 +0300

    UPD

--- battles/BattlePhase/UnitState.js
+++ battles/BattlePhase/UnitState.js
@@ -23,7 +23,7 @@ export class UnitState {
             initiative: h.initiative
         });
 
-        this.id = h.id;
+        this.id = h.heroId;
         this.team = h.team;
         
         this.templateId = h.templateId;
